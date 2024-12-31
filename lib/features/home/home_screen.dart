9bfed44a5283483058461f98d2fc1f11a466210a
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-12-30 22:08:59 -0300

    Add fcm integration (firebase push notification)

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,10 +1,47 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:challenge_with_flutter/router.gr.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    unawaited(() async {
+      final settings = await _firebaseMessaging.requestPermission();
+
+      if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+        String? token = await _firebaseMessaging.getToken();
+        if (token != null) {
+          print('Token del dispositivo: $token');
+        }
+      } else {
+        print('Permisos denegados para notificaciones');
+      }
+    }());
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Message data: ${message.data}');
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Message clicked! ${message.messageId}');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:challenge_with_flutter/router.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+Future<void> _backgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  // await setupFlutterNotifications();
+  // showFlutterNotification(message);
+  print("Handling a background message: ${message.messageId}");
+}
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
   final _appRouter = AppRouter();
   runApp(
     MaterialApp.router(
